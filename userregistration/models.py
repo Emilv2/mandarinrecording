8ec35e7f0d5cc16be63ac85c929dd080f931a39c
Emil Vanherp <emil.vanherp@gmail.com>
2016-09-04 17:00:45 +0800

    added Sex in registration form

--- userregistration/models.py
+++ userregistration/models.py
@@ -3,29 +3,42 @@ from django import forms
 from enum import Enum
 from django.contrib.auth.forms import UserCreationForm
 from django.utils.translation import ugettext, ugettext_lazy as _
+from .contributor import Contributor
 
-class Contributor(models.Model):
-    MALE = 'M'
-    FEMALE = 'F'
-    NO_ANSWER = 'N'
-    SEX_CHOISES = (
-            (MALE, 'Male'),
-            (FEMALE, 'Female'),
-            (NO_ANSWER, 'No_answer'),
-            )
-    sex = models.CharField(
-            max_length=2,
-            choices=SEX_CHOISES,
-            )
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    registration_date = models.DateTimeField()
-    email = models.EmailField()
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
 
-class ContributorCreationForm(UserCreationForm):
     email = forms.EmailField(
-            label=_("e-mail address"),
-            strip=True,
-            required=False,
-            widget=forms.EmailInput,
+           label=_("e-mail address"),
+           strip=True,
+           required=False,
+           widget=forms.EmailInput,
+           )
+
+    def save(self, commit=True):
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+class ContributorCreationForm(forms.ModelForm):
+
+    class Meta:
+        model = Contributor
+        fields = ['sex']
+
+    sex = forms.ChoiceField(
+            choices=Contributor.SEX_CHOISES + ((None, 'Please select one'),)
             )
+    def save(self, commit=True):
+        contributor = super(ContributorCreationForm, self).save(commit=False)
+        contributor.sex = self.cleaned_data["sex"]
+        if commit:
+            contributor.save()
+        return contributor
+
+
--- userregistration/views.py
+++ userregistration/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-from .models import ContributorCreationForm
+from .models import ContributorCreationForm, CustomUserCreationForm
 from django.template.context_processors import csrf
 from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
@@ -9,11 +9,16 @@ def registration(request):
     csrf_token = {}
     csrf_token.update(csrf(request))
 
-    form = ContributorCreationForm(request.POST)
-    if form.is_valid():
-        user = form.save()
+    user_form = CustomUserCreationForm(request.POST)
+    contributor_form = ContributorCreationForm(request.POST)
+    if all((user_form.is_valid(), contributor_form.is_valid())):
+        user = user_form.save()
+        contributor = contributor_form.save(commit=False)
+        contributor.user = user
+        contributor.save()
         return HttpResponseRedirect('/thanks/')
 
-    context = {'form' : form}
+    context = {'user_form' : user_form,
+            'contributor_form' : contributor_form}
 #TODO
     return render(request, 'registration.html', context)
