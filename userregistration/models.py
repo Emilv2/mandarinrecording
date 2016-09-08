d15859d4860666d3f066bbad2bfb8a5d7b0ec17a
Emil Vanherp <emil.vanherp@gmail.com>
2016-09-08 20:06:10 +0800

    feat: Use e-mail as username

--- userregistration/models.py
+++ userregistration/models.py
@@ -13,18 +13,19 @@ class CustomUserCreationForm(UserCreationForm):
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
 
-    email = forms.EmailField(
+    username = forms.EmailField(
             label=_("e-mail address"),
             strip=True,
             required=False,
             widget=forms.EmailInput,
             )
 
+
     def save(self, commit=True):
         user = super(CustomUserCreationForm, self).save(commit=False)
         user.first_name = self.cleaned_data["first_name"]
         user.last_name = self.cleaned_data["last_name"]
-        user.email = self.cleaned_data["email"]
+        user.username = self.cleaned_data["username"]
         if commit:
             user.save()
         return user
