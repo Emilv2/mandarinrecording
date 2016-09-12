21c176389a191b84435313497ba413b5d0e3d323
Emil Vanherp <emil.vanherp@gmail.com>
2016-09-12 22:49:34 +0800

    feat: Adds login page
    Adds a login page and redirects from recording to login if an user is not
    logged in

--- recording/views.py
+++ recording/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.template.context_processors import csrf
+from django.conf import settings
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
+@login_required()
 def recording(request):
     csrf_token = {}
     csrf_token.update(csrf(request))
--- userregistration/urls.py
+++ userregistration/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.registration, name='registration'),
+        url(r'^registration/', views.registration, name='registration'),
+        url(r'^login/', auth_views.login, {'template_name': 'login.html'}, name='login'),
+        url(r'^password_reset/', views.password_reset, name='password_reset'),
         ]
