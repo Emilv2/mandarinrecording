97f5c0ebcee70263cfdb578f7956d00d818699f0
Emil Vanherp <emil.vanherp@protonmail.ch>
2016-10-08 21:30:57 +0800

    fix: actually do request right
    Now no errors should be shown on initial page load.

--- userregistration/views.py
+++ userregistration/views.py
@@ -9,34 +9,44 @@ from django.contrib.auth import authenticate, login, views
 from django.contrib.auth import views as auth_views
 
 def registration(request):
-    csrf_token = {}
-    csrf_token.update(csrf(request))
-
     user_form = CustomUserCreationForm(request.POST)
     contributor_form = ContributorCreationForm(request.POST)
-
     captcha_list = [AudioCaptchaForm(request.POST) for i in range(3)]
-
-    if all((user_form.is_valid(), contributor_form.is_valid())) and \
-            all( i.is_valid() for i in captcha_list):
-        user = user_form.save()
-        contributor = contributor_form.save(commit=False)
-        contributor.user = user
-        contributor.save()
-        contributor = authenticate(
-                username=user_form.cleaned_data['username'],
-                password=user_form.cleaned_data['password1'],
-                )
-        login(request, contributor)
-        return HttpResponseRedirect('/recording/')
-
+    csrf_token = {}
+    csrf_token.update(csrf(request))
     audio_files = ['audio_file.mp3', 'audio_file.mp3', 'audio_file.mp3']
     context = {'user_form' : user_form,
             'contributor_form' : contributor_form,
             'captcha_list' : captcha_list,
             'audio_files' : audio_files}
-    #TODO
-    return render(request, 'registration.html', context)
+
+    if request.method == 'POST':
+        if all((user_form.is_valid(), contributor_form.is_valid())) and \
+                all( i.is_valid() for i in captcha_list):
+            user = user_form.save()
+            contributor = contributor_form.save(commit=False)
+            contributor.user = user
+            contributor.save()
+            contributor = authenticate(
+                    username=user_form.cleaned_data['username'],
+                    password=user_form.cleaned_data['password1'],
+                    )
+            login(request, contributor)
+            return HttpResponseRedirect('/recording/')
+        else:
+            return render(request, 'registration.html', context)
+    else:
+        user_form = CustomUserCreationForm()
+        contributor_form = ContributorCreationForm()
+        captcha_list = [AudioCaptchaForm() for i in range(3)]
+
+        audio_files = ['audio_file.mp3', 'audio_file.mp3', 'audio_file.mp3']
+        context = {'user_form' : user_form,
+                'contributor_form' : contributor_form,
+                'captcha_list' : captcha_list,
+                'audio_files' : audio_files}
+        #TODO
+        return render(request, 'registration.html', context)
 
 def login(request, *args, **kwargs):
     if request.user.is_authenticated():
