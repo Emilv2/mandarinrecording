179048d30546cc2acd87b9b94411f50f485c54ac
Emil Vanherp <emil.vanherp@protonmail.ch>
2017-02-06 04:10:46 +0100

    refactor: rename emilvanherp to mandarinrecording
    Website is not personal website but mandarinrecording website, so name
    is accordingly changed.

--- mandarinrecording/urls.py
+++ mandarinrecording/urls.py
@@ -1,4 +1,4 @@
-"""emilvanherp URL Configuration
+"""mandarinrecording URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
--- reset_password/views.py
+++ reset_password/views.py
@@ -7,7 +7,7 @@ from django.template import loader
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 from django.core.mail import send_mail
-from emilvanherp.settings import DEFAULT_FROM_EMAIL
+from mandarinrecording.settings import DEFAULT_FROM_EMAIL
 from django.views.generic import *
 from .forms import PasswordResetRequestForm, SetPasswordForm
 from django.contrib import messages
